2122f5e5fbc20578ee79be875a2dc238d5bb9b42
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-04-28 01:11:00 +0300

    Sprint 9. Java-Filmorate. Version 1

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.service.UserServiceImpl;
+import ru.yandex.practicum.filmorate.validation.ValidationException;
+import ru.yandex.practicum.filmorate.validation.Validator;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService = new UserServiceImpl();
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User newUser) {
+        try {
+            Validator.userValidator(newUser);
+            return new ResponseEntity<>(userService.saveUser(newUser), HttpStatus.CREATED);
+        } catch (ValidationException e) {
+            return new ResponseEntity<>(newUser, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User newUser) {
+        try {
+            Validator.userValidator(newUser);
+            return new ResponseEntity<>(userService.updateUser(newUser), HttpStatus.OK);
+        } catch (ValidationException e) {
+            return new ResponseEntity<>(newUser, HttpStatus.valueOf(500));
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> readAllUsers() {
+        return new ResponseEntity<>(userService.readAllUsers(), HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class Film {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Integer duration;
+
+
+}
