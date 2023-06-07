e937da892297754e2e8662e90c2e0e80cce22e9f
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-06-08 01:35:12 +0300

    v1.6 Codestyle для тестов Git

--- src/main/java/ru/yandex/practicum/filmorate/storage/MpaDBStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MpaDBStorage.java
@@ -26,13 +26,12 @@ public class MpaDBStorage implements MpaStorage {
     @Override
     public Mpa readById(Integer id) {
         String sqlQuery = "SELECT * FROM mpa WHERE id = ?";
-        if (id == null){
+        if (id == null) {
             throw new ValidationException("Невозможно выполнить запрос спустым аргументом.");
         }
-        try{
-            log.debug("Выведен жанр по id");
+        try {log.debug("Выведен жанр по id");
             return jdbcTemplate.queryForObject(sqlQuery, this::mapToMpa, id);
-        } catch (Throwable e){
+        } catch (Throwable e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Нет такого рейтинга");
         }
     }
@@ -40,11 +39,11 @@ public class MpaDBStorage implements MpaStorage {
     @Override
     public List<Mpa> readAll() {
         String sqlQuery = "SELECT * FROM mpa ORDER BY id";
-        log.debug ("Все рейтинги выведены");
+        log.debug("Все рейтинги выведены");
         return jdbcTemplate.query(sqlQuery, this::mapToMpa);
     }
 
-    public Mpa mapToMpa (ResultSet rs, int rowNum) throws SQLException {
+    public Mpa mapToMpa(ResultSet rs, int rowNum) throws SQLException {
         return Mpa.builder()
                 .id(rs.getInt("id"))
                 .name(rs.getString("name"))
